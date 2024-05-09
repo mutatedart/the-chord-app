6bc34a174ec7041052a57d99ce323d170ca311b0
eduardo.lessons <eduardo.lessons@gmail.com>
2024-05-09 14:57:17 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div className="nav">
+          <h1>theChord.app</h1>
+          <h4>by musicians for musicians</h4>
+          <a href="#" target=""><button class="nav-button">Songs</button></a>
+          <a href="#" target=""><button class="nav-button">Sets</button></a>
+        </div>
+        <div className="container">
+          <div className="paragraph">
+            Welcome! Here's how you can get started:
+          </div>
+          <div className="paragraph">
+            Click or Tap on the "Songs" button above to see what songs are in your
+            library, or get started building songs to add to your sets. Songs hold
+            sections, like the chorus, bridge, solo, or verse.
+          </div>
+          <div className="paragraph">
+            Click or Tap on the "Sets" button above in order to view or edit the
+            sets you've created. You can add or remove songs from sets, and name
+            them whatever you want, though we'd recommend something that will help
+            you recall what the set was for to begin with.
+          </div>
+          <div className="paragraph">
+            If you have questions or need support, please contact us by clicking the button below. Please
+            keep in mind that this is a tiny team, so it may take some time to
+            respond or investigate any issues you have.
+            <a href="#" target="_blank"><button className="btn">Contact</button></a>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
